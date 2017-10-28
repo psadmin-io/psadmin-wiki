eb60be9952b4c960c54a7ad83fed156bef60d51a
Dan Iverson <dan@daniverson.com>
2017-10-28 14:11:58 -0500

    Removing leading spaces and before URL

--- build/sidebar.sh
+++ build/sidebar.sh
@@ -1,4 +1,4 @@
 for file in ../posts/*/*.md
 do
-   awk -F'[:\n]' 'BEGIN { RS = "---"}  ; /title: /{ printf "%s: \"%s\"\n", $3, $7 }' $file 
+   awk -F'[:\n]' 'BEGIN { RS = "---"} ; /title: /{ printf "%s: \"%s\"\n", $3, $7 }' $file | sed 's/^ //' | sed 's/: \" /: \"/'
 done
